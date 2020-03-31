d8a97f27646b03188a507117a9f349b91fcd5edb
Danyihza <55189649+Danyihza@users.noreply.github.com>
2020-04-01 00:32:21 +0700

    asdsad

--- application/views/index.php
+++ application/views/index.php
@@ -37,14 +37,14 @@
 	</header>
 	<!-- END header -->
 
-	<section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(http://localhost/belenjehyu/assets/images/big_image_2.jpg);">
+	<section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(http://belenjehyu.highmoon.xyz/assets/images/pasar.jpg);">
 		<div class="container">
 			<div class="row align-items-center justify-content-center site-hero-inner">
 				<div class="col-md-10">
 
 					<div class="mb-5 element-animate">
 						<div class="block-17">
-							<h2 class="heading text-center mb-4">Find Oneline Courses That Suits You</h2>
+							<h2 class="heading text-center mb-4">Cari Pedagang Langganan Anda Disini</h2>
 							<form action="<?= base_url('main/cari'); ?>" method="get" class="d-block d-lg-flex mb-4">
 								<div class="fields d-block d-lg-flex">
 									<div class="textfield-search one-third"><input type="text" class="form-control" name="search" placeholder="Cari nama pedagang disini..."></div>
@@ -68,7 +68,9 @@
 		</div>
 	</section>
 	<!-- END section -->
-
+	<p class="heading text-center mb-4" style="font-size: 30px;
+  color: white;
+  font-weight: bold;">Hasil Pencarian</p>
 	<?php
 	foreach ($user as $u) :
 	?>
@@ -77,95 +79,19 @@
 				<img class="daging" src="<?= base_url('assets/'); ?>images/daging.png" width="46px"></div>
 			<div class="tengah">
 				<h3><?= $u['nama_pedagang']; ?></h3>
-				<a href="tel:<?= $u['kontak_pedagang']?>"><p><?= $u['kontak_pedagang']; ?></p></a>
+				<a href="tel:<?= $u['kontak_pedagang'] ?>">
+					<p><?= $u['kontak_pedagang']; ?></p>
+				</a>
 			</div>
 			<div class="kanan">
-				<a href="https://wa.me/62<?= $u['kontak_pedagang']?>?text=Halo+<?=$u['nama_pedagang']?>+%0D%0ASaya+ingin+membeli+sesuatu+di+toko+anda"><img class="whatsapp" src="<?= base_url('assets/'); ?>images/whatsapp.png" width="46px"></a>
+				<a href="https://wa.me/62<?= $u['kontak_pedagang'] ?>?text=Halo+<?= $u['nama_pedagang'] ?>+%0D%0ASaya+ingin+membeli+sesuatu+di+toko+anda"><img class="whatsapp" src="<?= base_url('assets/'); ?>images/whatsapp.png" width="46px"></a>
 			</div>
 		</div>
 	<?php endforeach; ?>
-
-
-
-	<!-- END section -->
-
-
+	<hr>
 	<footer class="site-footer">
 		<div class="container">
-			<div class="row mb-5">
-				<div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-					<h3>University</h3>
-					<p>Perferendis eum illum voluptatibus dolore tempora consequatur minus asperiores temporibus.</p>
-				</div>
-				<div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-					<h3 class="heading">Quick Link</h3>
-					<div class="row">
-						<div class="col-md-6">
-							<ul class="list-unstyled">
-								<li><a href="#">Home</a></li>
-								<li><a href="#">About Us</a></li>
-								<li><a href="#">Courses</a></li>
-								<li><a href="#">Pages</a></li>
-							</ul>
-						</div>
-						<div class="col-md-6">
-							<ul class="list-unstyled">
-								<li><a href="#">News</a></li>
-								<li><a href="#">Support</a></li>
-								<li><a href="#">Contact</a></li>
-								<li><a href="#">Privacy</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-					<h3 class="heading">Blog</h3>
-					<div class="block-21 d-flex mb-4">
-						<div class="text">
-							<h3 class="heading mb-0"><a href="#">Consectetur Adipisicing Elit</a></h3>
-							<div class="meta">
-								<div><a href="#"><span class="ion-android-calendar"></span> May 29, 2018</a></div>
-								<div><a href="#"><span class="ion-android-person"></span> Admin</a></div>
-								<div><a href="#"><span class="ion-chatbubble"></span> 19</a></div>
-							</div>
-						</div>
-					</div>
-					<div class="block-21 d-flex mb-4">
-						<div class="text">
-							<h3 class="heading mb-0"><a href="#">Dolore Tempora Consequatur</a></h3>
-							<div class="meta">
-								<div><a href="#"><span class="ion-android-calendar"></span> May 29, 2018</a></div>
-								<div><a href="#"><span class="ion-android-person"></span> Admin</a></div>
-								<div><a href="#"><span class="ion-chatbubble"></span> 19</a></div>
-							</div>
-						</div>
-					</div>
-					<div class="block-21 d-flex mb-4">
-						<div class="text">
-							<h3 class="heading mb-0"><a href="#">Perferendis eum illum</a></h3>
-							<div class="meta">
-								<div><a href="#"><span class="ion-android-calendar"></span> May 29, 2018</a></div>
-								<div><a href="#"><span class="ion-android-person"></span> Admin</a></div>
-								<div><a href="#"><span class="ion-chatbubble"></span> 19</a></div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
-					<h3 class="heading">Contact Information</h3>
-					<div class="block-23">
-						<ul>
-							<li><span class="icon ion-android-pin"></span><span class="text">203 Fake St. Mountain View, San
-									Francisco, California, USA</span></li>
-							<li><a href="#"><span class="icon ion-ios-telephone"></span><span class="text">+2 392 3929 210</span></a>
-							</li>
-							<li><a href="#"><span class="icon ion-android-mail"></span><span class="text">info@yourdomain.com</span></a></li>
-							<li><span class="icon ion-android-time"></span><span class="text">Monday &mdash; Friday 8:00am -
-									5:00pm</span></li>
-						</ul>
-					</div>
-				</div>
-			</div>
+
 			<div class="row pt-5">
 				<div class="col-md-12 text-center copyright">
 
@@ -174,15 +100,12 @@
 						Copyright &copy;<script>
 							document.write(new Date().getFullYear());
 						</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by
-						<a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+						<strong><a href="https://colorlib.com" target="_blank" class="text-white">Colorlib</a></strong>
 						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 					</p>
 					<p class="float-md-right">
-						<a href="#" class="fa fa-facebook p-2"></a>
-						<a href="#" class="fa fa-twitter p-2"></a>
-						<a href="#" class="fa fa-linkedin p-2"></a>
-						<a href="#" class="fa fa-instagram p-2"></a>
-
+						<a href="https://www.facebook.com/fahrizal.firdaus.129?sk=wall" class="fa fa-facebook p-2"></a>
+						<a href="https://www.instagram.com/ikirizal_/" class="fa fa-instagram p-2"></a>
 					</p>
 				</div>
 			</div>
