e23440a18a707b7e740ac7b7f71f7d4b3b4509e8
arinifir <55473280+arinifir@users.noreply.github.com>
2020-04-11 15:11:41 +0700

    s

--- db_pedagang(1).sql
+++ db_pedagang(1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2020 at 04:45 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
+-- Generation Time: 11 Apr 2020 pada 10.08
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `belenjehyu_db`
+-- Database: `db_pedagang`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Struktur dari tabel `admin`
 --
 
 CREATE TABLE `admin` (
@@ -36,7 +36,7 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `admin`
+-- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `admin` (`id`, `nama`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Struktur dari tabel `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -55,20 +55,20 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `kategori`
+-- Dumping data untuk tabel `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `gambar_kategori`) VALUES
-(1, 'Daging', ''),
-(2, 'Kue', ''),
-(3, 'Sayur', ''),
-(4, 'Buah', ''),
-(5, 'Sembako', '');
+(1, 'Daging', 'daging.png'),
+(2, 'Kue', 'kue.png'),
+(3, 'Sayur', 'sayur.png'),
+(4, 'Buah', 'buah.png'),
+(5, 'Sembako', 'sembako.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedagang`
+-- Struktur dari tabel `pedagang`
 --
 
 CREATE TABLE `pedagang` (
@@ -76,16 +76,24 @@ CREATE TABLE `pedagang` (
   `nama_pedagang` varchar(100) NOT NULL,
   `kontak_pedagang` varchar(15) NOT NULL,
   `tempat_pasar` varchar(200) NOT NULL,
-  `kategori` int(11) NOT NULL
+  `kategori` int(11) NOT NULL,
+  `foto_kios` varchar(255) NOT NULL,
+  `status_akun` int(11) NOT NULL,
+  `detail_pedagang` text NOT NULL,
+  `keterangan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `pedagang`
+-- Dumping data untuk tabel `pedagang`
 --
 
-INSERT INTO `pedagang` (`id_pedagang`, `nama_pedagang`, `kontak_pedagang`, `tempat_pasar`, `kategori`) VALUES
-(1, 'Maemunah', '081456789097', 'Pasar Tanjung', 5),
-(2, 'Joko', '082331147549', 'Pasar Semampir', 2);
+INSERT INTO `pedagang` (`id_pedagang`, `nama_pedagang`, `kontak_pedagang`, `tempat_pasar`, `kategori`, `foto_kios`, `status_akun`, `detail_pedagang`, `keterangan`) VALUES
+(1, 'Rizal', '082235255028', 'Pasar Pajarakan', 5, '', 0, '', 1),
+(3, 'Rinrin', '081335373470', 'Pasar Semampir', 2, '', 0, '', 1),
+(5, 'Ferrys', '083134920109', 'Pasar Semampir', 4, '', 0, '', 1),
+(6, 'Dany', '082331147549', 'Pasar Pajarakan', 1, '', 0, '', 1),
+(7, 'Yanti', '0821672612131', 'Pasar Semampir', 1, '20742670.jpg', 1, 'Daging ayam\r\ndaging sapi\r\nfftftfk\r\njyfg\r\nftyfjt\r\nat44tw34\r\ntyjt6\r\nvytfi\r\nftdr\r\nfi6fgyguhkiiuttftf\r\nfdtryd\r\nyjtftf\r\ngyg\r\ndrd]ugkyg\r\ndtrd', 1),
+(8, 'Mama', '77676597', 'Pasar Semampir', 2, 'clip_image00229.jpg', 1, 'berhasil diupdate kok', 1);
 
 --
 -- Indexes for dumped tables
@@ -120,25 +128,22 @@ ALTER TABLE `pedagang`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
 --
 -- AUTO_INCREMENT for table `pedagang`
 --
 ALTER TABLE `pedagang`
-  MODIFY `id_pedagang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pedagang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
 
 --
--- Constraints for dumped tables
---
-
---
--- Constraints for table `pedagang`
+-- Ketidakleluasaan untuk tabel `pedagang`
 --
 ALTER TABLE `pedagang`
   ADD CONSTRAINT `kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`id_kategori`);
